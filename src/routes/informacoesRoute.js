b19eb07da79f63c8822a03100e03ec2c2ce37b4e
Edufcc <edubcc15@gmail.com>
2023-05-31 21:32:05 -0300

    first commit

---
+++ src/routes/informacoesRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const informacoesController = require('../controllers/informacoesController');
+
+const router = express.Router();
+
+
+router.get('/1', informacoesController.getInformacoes);
+router.post('/1', informacoesController.createInformacoes);
+router.delete('/1', informacoesController.deleteInformacoes);
+
+module.exports = router
