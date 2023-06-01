24c6de4d4960cdcd243ef7eaa16e9283d85a7b56
Edufcc <edubcc15@gmail.com>
2023-06-01 20:30:50 -0300

    update

--- src/routes/informacoesRoute.js
+++ src/routes/informacoesRoute.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get('/1', informacoesController.getInformacoes);
 router.post('/1', informacoesController.createInformacoes);
+router.put('/', informacoesController.updateInformacoes);
 router.delete('/1', informacoesController.deleteInformacoes);
 
 module.exports = router
